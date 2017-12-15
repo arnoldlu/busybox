cb48b4cf0726f1be34afce49721a7000f8466bc6
Arnold Lu <arnoldlu@qq.com>
2017-12-15 22:39:41 +0800

    update build.sh
    Signed-off-by: Arnold Lu <arnoldlu@qq.com>

--- build.sh
+++ build.sh
@@ -5,3 +5,10 @@ export CROSS_COMPILE=arm-linux-gnueabi-
 make -j4 install
 #Configure _install folter P685
 sudo cp -r running_kernel_initramfs/* _install/
+sudo chmod +x _install/etc/init.d/rcS
+cd _install
+mkdir mnt
+mkdir dev
+cd dev
+sudo mknod console c 5 1
+sudo mknod null c 1 3
